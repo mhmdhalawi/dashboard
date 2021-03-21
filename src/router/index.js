976cd4059bbd8102d38fd352a48dd4c462aa5034
mhmdhalawi <mhmdhalawi@users.noreply.github.com>
2021-03-22 00:11:15 +0200

    fixed auth

--- src/router/index.js
+++ src/router/index.js
@@ -3,13 +3,6 @@ import Home from '../views/Home.vue';
 import About from '../views/About.vue';
 import Login from '../views/Login.vue';
 
-function getToken(to, from, next) {
-  const token = localStorage.getItem('token');
-  if (!token) {
-    next({ name: 'Login' });
-  } else next();
-}
-
 const routes = [
   {
     path: '/',
@@ -20,19 +13,24 @@ const routes = [
     path: '/about',
     name: 'About',
     component: About,
-    beforeEnter: getToken,
   },
   {
     path: '/home',
     name: 'Home',
     component: Home,
-    beforeEnter: getToken,
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const token = localStorage.getItem('token');
+  if (to.name !== 'Login' && !token) next({ name: 'Login' });
+  if (to.name == 'Login' && token) next({ name: 'Home' });
+  else next();
 });
 
 export default router;
